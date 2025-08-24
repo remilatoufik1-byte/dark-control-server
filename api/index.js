3060226fb0369445cca966f0b4b646dd9549df3b
remilatoufik1-byte <remilatoufik1@gmail.com>
2025-08-24 23:43:18 +0100

    Delete api/index.js
    Delete file

--- api/index.js
+++
@@ -1,7 +0,0 @@
-sexport default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ message: 'Dark Control Server is running ✅' });
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-}
